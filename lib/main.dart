41cc2598eaf56c5e42ca46f323464edab6e836c3
Alex <ZimenkovIUAI@gmail.com>
2020-08-08 00:16:20 +0300

    Сделал основное задание

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
-// void main() => runApp(MyApp());
-
-// void main() {
-//   runApp(
-//     MaterialApp(
-//       home: MyApp(),
-//     ),
-//   );
-// }
-
-// /// This Widget is the main application widget.
-// class MyApp extends StatelessWidget {
-//   static const String _title = 'ClickAndColor';
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: _title,
-//       home: MyStatefulWidget(),
-//       debugShowCheckedModeBanner: false,
-//     );
-//   }
-// }
-
-// final Shader linearGradient = LinearGradient(
-//   colors: <Color>[Color(0xffDA44bb), Color(0xff8921aa)],
-// ).createShader(Rect.fromLTWH(0.0, 0.0, 200.0, 70.0));
-
-// class MyStatefulWidget extends StatefulWidget {
-//   // MyStatefulWidget({Key key}) : super(key: key);
-
-//   @override
-//   _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
-// }
-
-// class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-//   // int _count = 0;
-
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Test Work. Zimenkov'),
-//       ),
-//       body: Center(
-//           child: Text(
-//         'Hey there',
-//         style: new TextStyle(
-//             fontSize: 36.0, foreground: Paint()..shader = linearGradient),
-//       )),
-
-//       backgroundColor: Colors.blue[500],
-//       // floatingActionButton: FloatingActionButton(
-//       // onPressed: () => setState(() => _count++),
-//       // tooltip: 'Increment Counter',
-//       // child: const Icon(Icons.add),
-//       // ),
-//     );
-//   }
-// }
-
 void main() {
   runApp(
     MaterialApp(
@@ -74,18 +14,127 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
-final Shader linearGradient = LinearGradient(
-  colors: <Color>[Color(0xffDA44bb), Color(0xff8921aa)],
-).createShader(Rect.fromLTWH(0.0, 100.0, 100.0, 670.0));
-
 class _MyAppState extends State<MyApp> {
-  List colors = [Colors.red, Colors.green, Colors.yellow];
-  Random random = new Random();
+  List colorsArr = [
+    Colors.red,
+    Colors.red[50],
+    Colors.red[100],
+    Colors.red[200],
+    Colors.red[300],
+    Colors.red[400],
+    Colors.red[500],
+    Colors.red[600],
+    Colors.red[700],
+    Colors.red[800],
+    Colors.red[900],
+    Colors.green,
+    Colors.green[100],
+    Colors.green[200],
+    Colors.green[300],
+    Colors.green[400],
+    Colors.green[500],
+    Colors.green[600],
+    Colors.green[700],
+    Colors.green[800],
+    Colors.green[900],
+    Colors.yellow,
+    Colors.yellow[100],
+    Colors.yellow[200],
+    Colors.yellow[300],
+    Colors.yellow[400],
+    Colors.yellow[500],
+    Colors.yellow[600],
+    Colors.yellow[700],
+    Colors.yellow[800],
+    Colors.yellow[900],
+    Colors.pink[100],
+    Colors.pink[200],
+    Colors.pink[300],
+    Colors.pink[400],
+    Colors.pink[500],
+    Colors.pink[600],
+    Colors.pink[700],
+    Colors.pink[800],
+    Colors.pink[900],
+    Colors.orange[100],
+    Colors.orange[200],
+    Colors.orange[300],
+    Colors.orange[400],
+    Colors.orange[500],
+    Colors.orange[600],
+    Colors.orange[700],
+    Colors.orange[800],
+    Colors.orange[900],
+    Colors.lime[100],
+    Colors.lime[200],
+    Colors.lime[300],
+    Colors.lime[400],
+    Colors.lime[500],
+    Colors.lime[600],
+    Colors.lime[700],
+    Colors.lime[800],
+    Colors.lime[900],
+    Colors.lightGreen[100],
+    Colors.lightGreen[200],
+    Colors.lightGreen[300],
+    Colors.lightGreen[400],
+    Colors.lightGreen[500],
+    Colors.lightGreen[600],
+    Colors.lightGreen[700],
+    Colors.lightGreen[800],
+    Colors.lightGreen[900],
+    Colors.teal[100],
+    Colors.teal[200],
+    Colors.teal[300],
+    Colors.teal[400],
+    Colors.teal[500],
+    Colors.teal[600],
+    Colors.teal[700],
+    Colors.teal[800],
+    Colors.teal[900],
+    Colors.cyan[100],
+    Colors.cyan[200],
+    Colors.cyan[300],
+    Colors.cyan[400],
+    Colors.cyan[500],
+    Colors.cyan[600],
+    Colors.cyan[700],
+    Colors.cyan[800],
+    Colors.cyan[900],
+    Colors.blue[100],
+    Colors.blue[200],
+    Colors.blue[300],
+    Colors.blue[400],
+    Colors.blue[500],
+    Colors.blue[600],
+    Colors.blue[700],
+    Colors.blue[800],
+    Colors.blue[900],
+    Colors.purple[100],
+    Colors.purple[200],
+    Colors.purple[300],
+    Colors.purple[400],
+    Colors.purple[500],
+    Colors.purple[600],
+    Colors.purple[700],
+    Colors.purple[800],
+    Colors.purple[900],
+    Colors.deepPurple[100],
+    Colors.deepPurple[200],
+    Colors.deepPurple[300],
+    Colors.deepPurple[400],
+    Colors.deepPurple[500],
+    Colors.deepPurple[600],
+    Colors.deepPurple[700],
+    Colors.deepPurple[800],
+    Colors.deepPurple[900],
+  ];
 
+  Random random = new Random();
   int index = 0;
 
   void changeIndex() {
-    setState(() => index = random.nextInt(3));
+    setState(() => index = random.nextInt(colorsArr.length - 1));
   }
 
   @override
@@ -98,9 +147,23 @@ class _MyAppState extends State<MyApp> {
         child: Text(
           'Hey there',
           style: new TextStyle(
-              fontSize: 36.0, foreground: Paint()..shader = linearGradient),
+            color: Colors.white,
+            fontSize: 36.0,
+            shadows: <Shadow>[
+              Shadow(
+                offset: Offset(0.0, 0.0),
+                blurRadius: 4.0,
+                color: Color.fromARGB(255, 0, 0, 0),
+              ),
+              Shadow(
+                offset: Offset(5.0, 5.0),
+                blurRadius: 15.0,
+                color: Color.fromARGB(200, 0, 0, 0),
+              ),
+            ],
+          ),
         ),
-        color: colors[index],
+        color: colorsArr[index],
       ),
     );
   }
